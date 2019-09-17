dea2869dfd513596d078a12d5a09a5324ce7b133
jg2018new <ads-132@163.com>
2019-09-17 08:40:57 +0800

    这里输入此次更新的信息

---
+++ src/main/java/com/example/demo/been/Person.java
@@ -0,0 +1,15 @@
+package com.example.demo.been;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+/**
+ * @Description
+ * @Author JG
+ * @Version V1.0.0
+ * @Date 2019-09-16
+ * @Time 21:00
+ */
+public class Person {
+    private String id;
+
+}
